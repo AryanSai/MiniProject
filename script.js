5739587c5060eccc8af006f71735aa4449c57785
Aryan Sai <aryansaiarvapelly@gmail.com>
2024-01-20 12:33:19 +0530

    dictionary

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 let current = 0,
   storyData,
-  audio;
+  audio,
+  dictionary;
 
 function updatePageContent() {
   const currentSlide = storyData.story.slides[current];
   document.getElementById("image").src = currentSlide.photo;
   document.getElementById("text").textContent = currentSlide.text;
+
+  //highlighting
+  // const keywords = currentSlide.keywords;
+  // console.log('Keywords for Slide:', keywords);
+
   if (audio) {
     audio.pause(); // stop the current before creating a new Audio object
     audio.currentTime = 0;
@@ -47,4 +53,16 @@ window.onload = () => {
       updatePageContent();
     })
     .catch((error) => console.error("Error fetching JSON:", error.message));
+
+  fetch("http://0.0.0.0:8000/Dictonary.json")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Failed to fetch Story JSON: ${response.statusText}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      dictionary = data;
+    })
+    .catch((error) => console.error("Error fetching Dictionary JSON:", error.message));
 };
