c19e7157a8697873393366c51e7eb36159c27894
Aryan Sai <aryansaiarvapelly@gmail.com>
2024-01-20 19:06:40 +0530

    highlighting done

--- script.js
+++ script.js
@@ -6,11 +6,10 @@ let current = 0,
 function updatePageContent() {
   const currentSlide = storyData.story.slides[current];
   document.getElementById("image").src = currentSlide.photo;
-  document.getElementById("text").textContent = currentSlide.text;
 
-  //highlighting
-  // const keywords = currentSlide.keywords;
-  // console.log('Keywords for Slide:', keywords);
+  const keywords = dictionary.words.map((wordObj) => wordObj.word);
+  const textElement = document.getElementById("text");
+  textElement.innerHTML = highlight(currentSlide.text, keywords);
 
   if (audio) {
     audio.pause(); // stop the current before creating a new Audio object
@@ -18,6 +17,19 @@ function updatePageContent() {
   }
   audio = new Audio(currentSlide.audio);
   playAudio();
+}
+
+function highlight(content, keywords) {
+  keywords.forEach((keyword) => {
+    const replacement = `<a href="Glossary.html#${encodeURIComponent(
+      keyword
+    )}" class="highlight" title="">${keyword}</a>`;
+
+    const regex = new RegExp(keyword, "ig");
+    content = content.replace(regex, replacement);
+  });
+
+  return content;
 }
 
 function playAudio() {
@@ -41,28 +53,31 @@ function prevPage() {
 }
 
 window.onload = () => {
-  fetch("http://0.0.0.0:8000/Stories/Story1/Story1.json")
-    .then((response) => {
+  const storyFetch = fetch(
+    "http://0.0.0.0:8000/Stories/Story1/Story1.json"
+  ).then((response) => {
+    if (!response.ok) {
+      throw new Error(`Failed to fetch JSON: ${response.statusText}`);
+    }
+    return response.json();
+  });
+
+  const dictionaryFetch = fetch("http://0.0.0.0:8000/Dictonary.json").then(
+    (response) => {
       if (!response.ok) {
-        throw new Error(`Failed to fetch JSON: ${response.statusText}`);
+        throw new Error(
+          `Failed to fetch Dictionary JSON: ${response.statusText}`
+        );
       }
       return response.json();
-    })
-    .then((data) => {
-      storyData = data;
+    }
+  );
+
+  Promise.all([storyFetch, dictionaryFetch])
+    .then(([storyDataResponse, dictionaryData]) => {
+      storyData = storyDataResponse; // Corrected assignment
+      dictionary = dictionaryData;
       updatePageContent();
     })
     .catch((error) => console.error("Error fetching JSON:", error.message));
-
-  fetch("http://0.0.0.0:8000/Dictonary.json")
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(`Failed to fetch Story JSON: ${response.statusText}`);
-      }
-      return response.json();
-    })
-    .then((data) => {
-      dictionary = data;
-    })
-    .catch((error) => console.error("Error fetching Dictionary JSON:", error.message));
 };
